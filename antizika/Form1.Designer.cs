3adb78741a19e229b40d723c8e26823718bf9f7a
Mari <marianar.ramalho.mr@gmail.com>
2025-03-26 15:18:52 -0300

    botoes

--- antizika/Form1.Designer.cs
+++ antizika/Form1.Designer.cs
@@ -29,21 +29,84 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            label1 = new Label();
             SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(255, 192, 255);
+            button1.Font = new Font("Arial Narrow", 24F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.Purple;
+            button1.Location = new Point(56, 168);
+            button1.Name = "button1";
+            button1.Size = new Size(189, 177);
+            button1.TabIndex = 0;
+            button1.Text = "Calcular Hash";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(255, 255, 192);
+            button2.Font = new Font("Arial Narrow", 24F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button2.ForeColor = Color.FromArgb(192, 192, 0);
+            button2.Location = new Point(351, 168);
+            button2.Name = "button2";
+            button2.Size = new Size(189, 177);
+            button2.TabIndex = 1;
+            button2.Text = "Escanear";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.FromArgb(255, 192, 128);
+            button3.Font = new Font("Arial Narrow", 24F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button3.ForeColor = Color.FromArgb(255, 128, 0);
+            button3.Location = new Point(659, 168);
+            button3.Name = "button3";
+            button3.Size = new Size(189, 177);
+            button3.TabIndex = 2;
+            button3.Text = "Escanear Pasta";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.FromArgb(192, 192, 255);
+            label1.Font = new Font("Arial Narrow", 36F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.Black;
+            label1.Location = new Point(313, 51);
+            label1.Name = "label1";
+            label1.Size = new Size(269, 57);
+            label1.TabIndex = 3;
+            label1.Text = "AntiVirus Zika";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
             ClientSize = new Size(926, 544);
+            Controls.Add(label1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
             FormBorderStyle = FormBorderStyle.FixedDialog;
             Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "Form1";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "AntiZika";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Label label1;
     }
 }
